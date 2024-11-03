efd104dee3e4c428c99009f61f85c6ef65d80023
mi-adesina <uniquecyborg@gmail.com>
2024-11-03 18:24:54 +0100

    count Occurrences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,19 @@
-function countOccurrences() {}
+/**
+ * Returns the number of occurrences of a character in a string.
+ * @param {string} str - The string to search.
+ * @param {string} char - The character to search for.
+ * @returns {number} - The number of occurrences of the character in the string.
+ */
+function countOccurrences(str, char) {
+  let occurrences = 0;
+
+  for (let index = 0; index < str.length; index++) {
+    if (str[index] === char) {
+      occurrences++;
+    }
+  }
+
+  return occurrences;
+}
 
 module.exports = countOccurrences;
