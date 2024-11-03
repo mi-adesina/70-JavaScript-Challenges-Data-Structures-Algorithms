e5a1b80a060327c2dd7c2b19e0306051e295fe58
mi-adesina <uniquecyborg@gmail.com>
2024-11-03 18:10:18 +0100

    calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,22 @@
-function calculator() {}
+/**
+ * Returns the result of a calculation.
+ * @param {number} num1 - The first number.
+ * @param {number} num2 - The second number.
+ * @param {string} operator - The operator to use in the calculation.
+ * @returns {number} - The result of the calculation.
+ */
+function calculator(num1, num2, operator) {
+  if (operator === '+') {
+    return num1 + num2;
+  } else if (operator === '-') {
+    return num1 - num2;
+  } else if (operator === '*') {
+    return num1 * num2;
+  }  else if (operator === '/') {
+    return num1 / num2;
+  } else {
+    throw new Error("'Invalid operator'");
+  }
+}
 
 module.exports = calculator;
