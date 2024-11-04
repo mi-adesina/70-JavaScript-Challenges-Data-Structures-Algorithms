aa3a382aa8810036db1784328bcce047a50e0cda
mi-adesina <uniquecyborg@gmail.com>
2024-11-04 19:05:29 +0100

    05-find-max-number

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,22 @@
-function findMaxNumber() {}
+/**
+ * Returns the largest number in an array.
+ * @param {number[]} arr - The array of numbers.
+ * @returns {number} - The largest number in the array.
+ */
+function findMaxNumber(arr) {
+  if (!arr.length) {
+    return undefined;
+  }
+
+  let max = arr[0];
+
+  for (let index = 1; index < arr.length; index++) {
+    if (arr[index] > max) {
+      max = arr[index];
+    }
+  }
+
+  return max;
+}
 
 module.exports = findMaxNumber;
