f88b04d963cbdabdf4d1accda2d9aeb0fdd70ac0
mi-adesina <uniquecyborg@gmail.com>
2024-11-04 19:48:12 +0100

    06-title-case

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,40 @@
-function titleCase() {}
+// /**
+//  * Returns a string with the first letter of each word capitalized.
+//  * @param {string} str - The string to capitalize.
+//  * @returns {string} - The string with the first letter of each word capitalized.
+//  */
+// function titleCase(str) {
+//   if (!str) {
+//     return undefined;
+//   }
+
+//   let retVal = '';
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (i === 0) {
+//       retVal += str[i].toUpperCase();
+//     } else if (str[i - 1]  === ' ') {
+//       retVal += str[i].toUpperCase();
+//     } else {
+//       retVal += str[i].toLowerCase();
+//     }
+//   }
+
+//   return retVal;
+// }
+
+/**
+ * Returns a string with the first letter of each word capitalized.
+ * @param {string} str - The string to capitalize.
+ * @returns {string} - The string with the first letter of each word capitalized.
+ */
+function titleCase(str) {
+  if (!str) return undefined;
+
+  return str
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+}
 
 module.exports = titleCase;
