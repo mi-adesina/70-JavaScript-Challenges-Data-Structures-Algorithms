5221e9687b8687a93a4bf919c18efd446b0101ed
mi-adesina <uniquecyborg@gmail.com>
2024-11-08 03:28:07 +0100

    09-count-vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,24 @@
-function countVowels() {}
+/**
+ * Returns the number of vowels in a string.
+ * @param {string} str - The string to search.
+ * @returns {number} - The number of vowels in the string.
+ */
+function countVowels(str) {
+  if (typeof str !== 'string') {
+    return undefined;
+  }
+
+  const vowels = 'aeiou';
+  const formattedStr = str.toLowerCase();
+  let vowelCounts = 0;
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    if (vowels.includes(formattedStr[i])) {
+      vowelCounts++;
+    }
+  }
+
+  return vowelCounts;
+}
 
 module.exports = countVowels;
