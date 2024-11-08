a76c20c37d5e258dcd1a8810b85f7ee10b20b5a3
mi-adesina <uniquecyborg@gmail.com>
2024-11-08 03:17:34 +0100

    07-reverse-string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,20 @@
-function reverseString() {}
+/**
+ * Returns the reverse of a string.
+ * @param {string} str - The string to reverse.
+ * @returns {string} - The reverse of the string.
+ */
+function reverseString(str) {
+  if (typeof str !== 'string') {
+    return undefined;
+  }
+
+  let reversed = '';
+
+  for (let i = 0; i < str.length; i++) {
+    reversed += str[str.length - 1 - i];
+  }
+
+  return reversed;
+}
 
 module.exports = reverseString;
