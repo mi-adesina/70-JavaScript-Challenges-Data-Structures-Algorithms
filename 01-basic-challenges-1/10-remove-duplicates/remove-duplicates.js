2a85b50d9fed0e9a934530950f11cf2b0261f601
mi-adesina <uniquecyborg@gmail.com>
2024-11-08 03:57:41 +0100

    10-remove-duplicates

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,14 @@
-function removeDuplicates() {}
+/**
+ * Returns a new array with duplicates removed.
+ * @param {any[]} arr - The array to remove duplicates from.
+ * @returns {any[]} - The new array with duplicates removed.
+ */
+function removeDuplicates(arr) {
+  if (!Array.isArray(arr)) {
+    return undefined;
+  }
+  
+  return [...new Set(arr)];
+}
 
 module.exports = removeDuplicates;
