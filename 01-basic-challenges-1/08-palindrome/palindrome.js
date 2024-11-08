1bd5f65e40dd0e5f1cfed6ae415ea089d35686a8
mi-adesina <uniquecyborg@gmail.com>
2024-11-08 03:21:33 +0100

    08-palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,26 @@
-function isPalindrome() {}
+/**
+ * Returns true if the string is a palindrome.
+ * @param {string} str - The string to check.
+ * @returns {boolean} - True if the string is a palindrome, false otherwise.
+ */
+function isPalindrome(str) {
+  if (typeof str !== 'string') {
+    return undefined;
+  }
+
+  const testStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  if (testStr === '') {
+    return false;
+  }
+
+  for (let i = 0; i < testStr.length / 2; i++) {
+    if (testStr[i] != testStr[testStr.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = isPalindrome;
